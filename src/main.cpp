034053b6c2a69ac1f9088c0d7189b567dac61897
depuc8 <praveendeepak8koripalli@gmail.com>
2026-08-16 21:37:31 +0530

    test: included a testing driver to check if it is working as intended

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
-#include<fstream>
+#include<string>
 #include "../include/Ybus.h"
+
+using ComplexMatrix = std::vector<std::vector<std::complex<double>>>;
 
 int main(){
 
@@ -8,13 +10,35 @@ int main(){
     std::string file_path;
     std::cin >> file_path; 
 
-    std::ifstream file(file_path);
+    ComplexMatrix ybus_0 = Ybus(file_path,0);
+    std::cout<<"returns Ybus and Outputs nothing"<<std::endl;
+    std::cout<<"\n";
+    ComplexMatrix ybus_1 = Ybus(file_path,1);
+    std::cout<<"\n";
+    std::cout<<"\nreturns Ybus and prints Ybus in the terminal"<<std::endl;
 
-    
-    if(!file.is_open()){
-        std::cerr<<"Error: could not find the file\n";
-        return 1;
-    }
-    Ybus(file,file_path);
+    std::cout<<"\n";
+
+    ComplexMatrix ybus_2 = Ybus(file_path,2);
+
+    std::cout<<"\n";
+    std::cout<<"\nreturns Ybus and writes Ybus to a file in the file_path"<<std::endl;
+
+    std::cout<<"\n";
+    ComplexMatrix ybus_3 = Ybus(file_path,3);
+
+    std::cout<<"\n";
+    std::cout<<"\ndoes all of the above"<<std::endl;
+
+    std::cout<<"\n";
+
+    std::cout<<ybus_0[0][0]<<std::endl;
+    std::cout<<ybus_1[0][0]<<std::endl;
+    std::cout<<ybus_2[0][0]<<std::endl;
+    std::cout<<ybus_3[0][0]<<std::endl;
+
+    std::cout<<"\nall the options return Ybus"<<std::endl;
+
+    return 0;
 }
 
